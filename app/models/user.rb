31cd0d4aa9e0dfc9eef555882848c9d01ea8e264
Sandstorm750 <sandstorm750@gmail.com>
2019-11-18 23:50:14 +0500

    long road

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def level_tests(level)
+    Test.joins('JOIN user_tests ON user_tests.test_id = tests.id')
+    .where('user_tests.user_id = ? AND tests.level = ?', self.id, level)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_21_181622) do
+ActiveRecord::Schema.define(version: 2019_11_18_165924) do
 
   create_table "answers", force: :cascade do |t|
     t.string "text", null: false
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(version: 2019_10_21_181622) do
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "category_id"
+  end
+
+  create_table "user_tests", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
