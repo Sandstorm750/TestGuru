8b1155aecbac20f5b304ad60da3b948a73bd513e
Sandstorm750 <sandstorm750@gmail.com>
2019-12-25 00:15:02 +0500

    Additive to No. 6

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,18 +5,7 @@ class Answer < ApplicationRecord
 
   scope :correct_answer, -> { where(correct: true)}
 
-  # def quest
-  #   question.answers.include(question_id: question.id)
-  #
-  # end
-
-  # quest = question.answers.include(question_id: question.id)
-
   private
-
-  # def validate_diapason
-  #   errors.add(:question) unless (1..4).include? (Answer.where(question_id = question.id))
-  # end
 
   def validate_diapason
     errors.add(:question) unless (0..4).include? question.answers.count
